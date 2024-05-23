23cfca1abc040097a1c92284a0581122c29c8385
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-05-23 19:01:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,29 +3,29 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./api/routes/auth.routes.js";
+import messageRoutes from "./api/routes/message.routes.js";
+import userRoutes from "./api/routes/user.routes.js";
 
-import connectToMongoDB from "./db/connectToMongoDB.js";
-import { app, server } from "./socket/socket.js";
+import connectToMongoDB from "./api/db/connectToMongoDB.js";
+import { app, server } from "./api/socket/socket.js";
 
 dotenv.config();
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 server.listen(PORT, () => {
